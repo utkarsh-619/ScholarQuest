d7cb3a4689e90e4379e5ed2231f14a70300e6d74
vishaldevasics <vishaldevasics@gmail.com>
2024-11-03 02:54:17 +0530

    added sidebar and created client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import SignIn from "./pages/auth/singin";
+// import SideMenu from "./components/sidemenu";
+
+function App() {
+  return (
+    <div>
+      <SignIn /> 
+      {/* <SideMenu /> */}
+    </div>
+  );
+}
+
+export default App;
